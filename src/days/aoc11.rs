ad2972f9f79e9705f76d125b2c5153ec23bd7d35
Andri Florin <andriflorin@me.com>
2024-12-02 13:40:10 +0100

    added day 3-12 template

---
+++ src/days/aoc11.rs
@@ -0,0 +1,10 @@
+use std::cmp::{PartialEq};
+
+
+pub fn part_a(data: Vec<&str>) -> i32 {
+    -1
+}
+
+pub fn part_b(data: Vec<&str>) -> i32 {
+    -1
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -3,6 +3,16 @@ use std::io::{self, BufRead, BufReader};
 
 mod aoc01;
 mod aoc02;
+mod aoc03;
+mod aoc04;
+mod aoc05;
+mod aoc06;
+mod aoc07;
+mod aoc08;
+mod aoc09;
+mod aoc10;
+mod aoc11;
+mod aoc12;
 
 pub fn run(day: i32, first: bool, test: bool) -> Result<i32, io::Error> {
     let data = load_data(day, test)?;
@@ -13,6 +23,26 @@ pub fn run(day: i32, first: bool, test: bool) -> Result<i32, io::Error> {
         (1, false) => aoc01::aoc01_b(data_refs),
         (2, true) => aoc02::aoc02_a(data_refs),
         (2, false) => aoc02::aoc02_b(data_refs),
+        (3, true) => aoc03::part_a(data_refs),
+        (3, false) => aoc03::part_b(data_refs),
+        (4, true) => aoc04::part_a(data_refs),
+        (4, false) => aoc04::part_b(data_refs),
+        (5, true) => aoc05::part_a(data_refs),
+        (5, false) => aoc05::part_b(data_refs),
+        (6, true) => aoc06::part_a(data_refs),
+        (6, false) => aoc06::part_b(data_refs),
+        (7, true) => aoc07::part_a(data_refs),
+        (7, false) => aoc07::part_b(data_refs),
+        (8, true) => aoc08::part_a(data_refs),
+        (8, false) => aoc08::part_b(data_refs),
+        (9, true) => aoc09::part_a(data_refs),
+        (9, false) => aoc09::part_b(data_refs),
+        (10, true) => aoc10::part_a(data_refs),
+        (10, false) => aoc10::part_b(data_refs),
+        (11, true) => aoc11::part_a(data_refs),
+        (11, false) => aoc11::part_b(data_refs),
+        (12, true) => aoc12::part_a(data_refs),
+        (12, false) => aoc12::part_b(data_refs),
         _ => -1,
     };
 
