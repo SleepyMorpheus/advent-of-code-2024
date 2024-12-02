f5fd41ae257f1c8734042ca373ba858e94309e48
Andri Florin <andriflorin@me.com>
2024-12-02 10:38:54 +0100

    switched to microseconds for benchmark

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@
 for a in {1..24}; do
   # Iterate over b from 0 to 1
   for b in {0..1}; do
-    hyperfine -m 1000 -N --warmup 100 "target/release/aoc2024 $a $b 0 0" 2> /dev/null
+    hyperfine -m 1000 -N --warmup 100 --time-unit microsecond "target/release/aoc2024 $a $b 0 0" 2> /dev/null
   done
 done
