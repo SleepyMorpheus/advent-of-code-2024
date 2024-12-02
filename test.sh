296e9ad8255a2663a0dd48062c07c917c1786bab
Andri Florin <andriflorin@me.com>
2024-12-02 10:50:14 +0100

    better benchmarking output

--- test.sh
+++ test.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# Iterate over a from 1 to 24
+echo "| Day (a) | Part 1 (b=0) | Part 2 (b=1) |"
+echo "|---------|--------------|--------------|"
+
+extract_mean_time() {
+  local result=$(hyperfine -m 1000 -N --warmup 10 "$1" --time-unit microsecond 2>/dev/null | grep "Time (mean ± σ)")
+  local mean_time=$(echo "$result" | awk '{print $5}')
+  echo "$mean_time µs"
+}
+
 for a in {1..24}; do
-  # Iterate over b from 0 to 1
-  for b in {0..1}; do
-    hyperfine -m 1000 -N --warmup 100 --time-unit microsecond "target/release/aoc2024 $a $b 0 0" 2> /dev/null
-  done
+  command_b0="target/release/aoc2024 $a 0 0 0"
+  command_b1="target/release/aoc2024 $a 1 0 0"
+
+  part1_time=$(extract_mean_time "$command_b0")
+  part2_time=$(extract_mean_time "$command_b1")
+
+  echo "| $a       | $part1_time     | $part2_time     |"
 done
