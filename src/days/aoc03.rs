eb1be40a0ea2e37c04f5c292ef77120788cb5659
Andri Florin <andriflorin@me.com>
2024-12-03 15:32:43 +0100

    replaced regex dependency with custom one

--- src/days/aoc03.rs
+++ src/days/aoc03.rs
@@ -1,44 +1,49 @@
-use regex_lite::Regex;
-use std::cmp::PartialEq;
 use crate::lib::input_parser::load_as_string;
 
-pub fn part_a(path: String) -> i32 {
-    let regex = Regex::new(r"mul\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\)").unwrap();
-    let mut sum = 0;
-    for occ in regex.find_iter(&*load_as_string(path)) {
-        let mut parts = occ.as_str().replace("mul(", "").replace(")", "");
-        let mut parts = parts.split(",");
-        sum += parts.next().unwrap().parse::<i32>().unwrap()
-            * parts.next().unwrap().parse::<i32>().unwrap()
-    }
-    sum
+fn part_a_helper(data: String) -> i32 {
+    data.split("mul(")
+        .skip(1)
+        .map(|x| parse_mul(x))
+        .sum()
 }
 
-#[derive(PartialEq)]
-enum Mode {
-    DO,
-    DONT,
+pub fn part_a(path: String) -> i32 {
+    part_a_helper(load_as_string(path))
 }
+
 
 pub fn part_b(path: String) -> i32 {
-    let mut sum = 0;
-    let mut prev_instr = Mode::DO;
-    let regex_mul = Regex::new(r"(mul\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\))|(do\(\))|don't\(\)").unwrap();
-    for occ in regex_mul.find_iter(&*load_as_string(path)) {
-        let occ = occ.as_str();
-        match (occ) {
-            "do()" => prev_instr = Mode::DO,
-            "don't()" => prev_instr = Mode::DONT,
-            _ => {
-                if prev_instr == Mode::DONT {
-                    continue;
-                }
-                let mut parts = occ.replace("mul(", "").replace(")", "");
-                let mut parts = parts.split(",");
-                sum += parts.next().unwrap().parse::<i32>().unwrap()
-                    * parts.next().unwrap().parse::<i32>().unwrap()
+    load_as_string(path).split("do()").map(|x| {
+        part_a_helper(x.split_once("don't()")
+            .map_or(x.to_string(),|(y, _)| y.to_string()))
+    }).sum::<i32>()
+}
+
+// Accepts the part that follows mul( and returns the product of the two numbers
+pub fn parse_mul(txt: &str) -> i32 {
+    let mut chars = txt.chars();
+    let mut num_a = String::new();
+    let mut num_b = String::new();
+
+    while let Some(digit) = chars.next() {
+        if !digit.is_digit(10) {
+            if digit != ',' {
+                return 0;
             }
+            break;
         }
+        num_a.push(digit);
     }
-    sum
+
+    while let Some(digit) = chars.next() {
+        if !digit.is_digit(10) {
+            if digit != ')' {
+                return 0;
+            }
+            break;
+        }
+        num_b.push(digit);
+    }
+
+    num_a.parse::<i32>().unwrap() * num_b.parse::<i32>().unwrap()
 }
