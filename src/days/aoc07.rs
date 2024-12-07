6c3ab9d8263df109288d6f242fb060fde333d491
Andri Florin <andriflorin@me.com>
2024-12-07 17:39:05 +0100

    Revert "day7 - using rayon"
    This reverts commit bbf362e85d4d832829772be9b0f04a5e33b51065.

--- src/days/aoc07.rs
+++ src/days/aoc07.rs
@@ -1,24 +1,24 @@
-use crate::helper::input_parser::load_as_vec;
+use crate::helper::input_parser::{load_as_vec, load_matrix_chars, load_matrix_two};
 use rayon::iter::ParallelIterator;
-use rayon::prelude::IntoParallelRefIterator;
+use rayon::prelude::IntoParallelIterator;
+use std::collections::HashSet;
+use log::log;
 
 fn parse_line(line: &str) -> (i64, Vec<i64>) {
     let (left, right) = line.split_once(": ").unwrap();
-    let right = right
-        .split(" ")
-        .map(|x| x.parse::<i64>().unwrap())
-        .collect();
+    let right = right.split(" ").map(|x| x.parse::<i64>().unwrap()).collect();
     (left.parse::<i64>().unwrap(), right)
 }
 
 enum Op {
     M,
     A,
+    C
 }
 
 fn remove_suffix_if_matches(left: u64, right: u64) -> Option<u64> {
     // ignoring case if right == 0 :)
-
+    
     let digits = right.ilog10() + 1;
     let modulus = 10u64.pow(digits);
 
@@ -31,25 +31,29 @@ fn remove_suffix_if_matches(left: u64, right: u64) -> Option<u64> {
 
 fn recursive(num: &[i64], curr: i64, last_op: Op, allow_concat: bool) -> bool {
     match num.split_first() {
-        None => match last_op {
-            Op::A => curr == 0,
-            Op::M => curr == 1,
+        None => {
+            match last_op {
+                Op::A => curr == 0,
+                Op::M => curr == 1,
+                Op::C => true,
+            }
         },
         Some((x, xs)) => {
+
             if let Some(suffix) = remove_suffix_if_matches(curr as u64, *x as u64) {
                 if allow_concat && recursive(xs, suffix as i64, last_op, allow_concat) {
-                    return true;
+                    return true
                 }
             }
 
             if curr % x == 0 {
                 if recursive(xs, curr / x, Op::M, allow_concat) {
-                    return true;
+                    return true
                 }
             }
             if curr - x >= 0 {
                 if recursive(xs, curr - x, Op::A, allow_concat) {
-                    return true;
+                    return true
                 }
             }
             false
@@ -57,34 +61,30 @@ fn recursive(num: &[i64], curr: i64, last_op: Op, allow_concat: bool) -> bool {
     }
 }
 
-pub fn part_a(path: String) -> i64 {
-    let result: i64 = load_as_vec(path)
-        .par_iter()
-        .map(|line| {
-            let (left, mut right) = parse_line(line);
-            right.reverse();
-            let res = recursive(&*right, left, Op::M, false);
-            match res {
-                true => left,
-                false => 0,
-            }
-        })
-        .sum();
-    result
+pub fn part_a(path: String) -> i32 {
+    let result = load_as_vec(path).iter().fold(0, |acc, line| {
+        let (left, mut right) = parse_line(line);
+        right.reverse();
+        let res = recursive(&*right, left, Op::M, false);
+        match res {
+            true => acc + left,
+            false => acc
+        }
+    });
+    // println!("{}", result);
+    -2
 }
 
-pub fn part_b(path: String) -> i64 {
-    let result: i64 = load_as_vec(path)
-        .par_iter()
-        .map(|line| {
-            let (left, mut right) = parse_line(line);
-            right.reverse();
-            let res = recursive(&*right, left, Op::M, true);
-            match res {
-                true => left,
-                false => 0,
-            }
-        })
-        .sum();
-    result
+pub fn part_b(path: String) -> i32 {
+    let result = load_as_vec(path).iter().fold(0, |acc, line| {
+        let (left, mut right) = parse_line(line);
+        right.reverse();
+        let res = recursive(&*right, left, Op::M, true);
+        match res {
+            true => acc + left,
+            false => acc
+        }
+    });
+    // println!("{}", result);
+    -2
 }
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -9,22 +9,22 @@ mod aoc05;
 mod aoc06;
 mod aoc07;
 
-pub fn run(day: i32, first: bool, test: bool) -> Result<i64, io::Error> {
+pub fn run(day: i32, first: bool, test: bool) -> Result<i32, io::Error> {
     let path = resolve_path(day, test);
 
     let output = match (day, first) {
-        (1, true) => aoc01::aoc01_a(path) as i64,
-        (1, false) => aoc01::aoc01_b(path) as i64,
-        (2, true) => aoc02::aoc02_a(path) as i64,
-        (2, false) => aoc02::aoc02_b(path) as i64,
-        (3, true) => aoc03::part_a(path) as i64,
-        (3, false) => aoc03::part_b(path) as i64,
-        (4, true) => aoc04::part_a(path) as i64,
-        (4, false) => aoc04::part_b(path) as i64,
-        (5, true) => aoc05::part_a(path) as i64,
-        (5, false) => aoc05::part_b(path) as i64,
-        (6, true) => aoc06::part_a(path) as i64,
-        (6, false) => aoc06::part_b(path) as i64,
+        (1, true) => aoc01::aoc01_a(path),
+        (1, false) => aoc01::aoc01_b(path),
+        (2, true) => aoc02::aoc02_a(path),
+        (2, false) => aoc02::aoc02_b(path),
+        (3, true) => aoc03::part_a(path),
+        (3, false) => aoc03::part_b(path),
+        (4, true) => aoc04::part_a(path),
+        (4, false) => aoc04::part_b(path),
+        (5, true) => aoc05::part_a(path),
+        (5, false) => aoc05::part_b(path),
+        (6, true) => aoc06::part_a(path),
+        (6, false) => aoc06::part_b(path),
         (7, true) => aoc07::part_a(path),
         (7, false) => aoc07::part_b(path),
         _ => -1,
